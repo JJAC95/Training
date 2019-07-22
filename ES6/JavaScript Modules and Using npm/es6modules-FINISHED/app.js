ab6bcd0de1559ae515de7efb454e6c6d5413059d
JJAC95 <jackbradbourne@Jacks-MacBook-Pro.local>
2019-07-22 09:54:35 +0100

    22/07

--- ES6/JavaScript Modules and Using npm/es6modules-FINISHED/app.js
+++ ES6/JavaScript Modules and Using npm/es6modules-FINISHED/app.js
@@ -1,11 +1,11 @@
-// import { uniq } from 'lodash';
+import { uniq } from 'lodash';
 import insane from 'insane';
 import jsonp from 'jsonp';
-import { apiKey as key, url, sayHi, old, dog } from './src/config';
-import apiKey from './src/config';
+// import { apiKey as key, url, sayHi, old, dog } from './src/config';
 import User, { createURL, gravatar } from './src/user';
-console.log(apiKey);
+import { apiKey as key, url, sayHi, old, dog } from './src/config';
 
+console.log(key, url, sayHi('Jack'), old, dog);
 const Jack = new User('Wes Bos', 'wesbos@gmail.com', 'wesbos.com');
 const profile = createURL(Jack.name);
 const image = gravatar(Jack.email);
